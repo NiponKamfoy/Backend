4c5783001d886837c98ddf4165df8c65e17df67c
NiponKamfoy <s6201012630061@email.kmutnb.ac.th>
2023-02-05 18:32:50 +0700

    add prepare data modules

--- main.py
+++ main.py
@@ -59,10 +59,7 @@ async def user_login(loginitem:LoginItem):
 
 @app.get('/get_index/{data_index}&{p_name}&{date}&{index_folder}')
 def getGridSpei(data_index: str, index_folder: str, p_name: str, date:str = '2006-01', x_access_token: Union[list[str], None] = Header(default=None)):
-    test = data['key']
     if (x_access_token != None and x_access_token[0] == data['key']):
         temp = convert_nc_json(p_name, date, data_index, index_folder)
-        # response = jsonify(temp)
-        # response.headers.add('Access-Control-Allow-Origin', '*')
         return temp
     return x_access_token == data['key']
