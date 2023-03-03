e6b795f19ab5b3d5400f7a6f377e7b0a137ee6f3
NiponKamfoy <s6201012630061@email.kmutnb.ac.th>
2023-03-03 10:42:13 +0700

    update allow method on CORS

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app.add_middleware(
    CORSMiddleware,
     allow_origins = ["*"],
     allow_credentials =True,
-    allow_methods = ["*"],
+    allow_methods = ["POST","GET","DELETE", "PUT"],
     allow_headers= ["*"],
 )
 
